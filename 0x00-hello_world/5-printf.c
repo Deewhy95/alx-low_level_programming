c17b8f28ed608aff1d11667cd363250171a7375b
Deewhy95 <aosifuye@yahoo.com>
2022-09-08 09:58:34 -0700

    Updated Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly with proper grammar, but the outcome is a  piece of art,
+ * main - prints exactly with proper grammar, 
+ * but the outcome is a  piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
