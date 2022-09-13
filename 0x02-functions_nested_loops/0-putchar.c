dbd1dcbd7d11088d18d12d40f646de20495ee66a
Deewhy95 <aosifuye@yahoo.com>
2022-09-13 06:55:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main -  prints EMMAnue, followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_putchar('E');
+	_putchar('M');
+	_putchar('M');
+	_putchar('A');
+	_putchar('n');
+	_putchar('u');
+	_putchar('e');
+	_putchar('\n');
+
+												return (0);
+}
