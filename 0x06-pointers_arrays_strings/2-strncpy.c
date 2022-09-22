7617f649c52f0c98079cb132db32a9b9eed46f11
Deewhy95 <aosifuye@yahoo.com>
2022-09-22 09:12:43 -0700

    task 4

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _strncpy - copies a string
- * @dest: destnation string
+ * @dest: destination string
  * @src: source string
  * @n: number of bytes to copy
  *
@@ -16,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[i] != '\0' && i < n)
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
 		i++;
 	}
 
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s2: second string to compare
  *
  * Return: less than 0 if s1 is less than s2, 0 if they're equal,
- * mre than 0 if s1 is greater than s2
+ * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -16,8 +16,8 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (0);
 		}
-	s1++;
-	s2++;
+		s1++;
+		s2++;
 	}
-
-}	
+	return (*s1 - *s2);
+}
