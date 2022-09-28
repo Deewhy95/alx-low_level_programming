2d2de9ba7ccd92014cbf340fc905f2efbb2e47bf
Deewhy95 <aosifuye@yahoo.com>
2022-09-28 11:49:30 -0700

    Update task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  */
@@ -9,7 +9,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s +1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
