52ae10ce11abe0d0ae1ebcae147e2f731d0c3a08
Deewhy95 <aosifuye@yahoo.com>
2022-09-29 00:04:01 -0700

    Task power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,10 +2,11 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x; value to raise
+ * @x: value to raise
  * @y: power
  *
- * Return: result of the power
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _pow_recursion(int x, int y)
